0f8987fa1415fb5e0ad39d1852b30638e75ed89e
ClementScordia <clement.scordia@gmail.com>
2018-02-12 11:51:25 +0100

    Update Reponse.java

--- Classes/Reponse.java
+++ Classes/Reponse.java
@@ -1,78 +1,34 @@
 package projetsportif;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Reponse {
-	public int rep_id;
-	private static int i = 0;
-	public boolean rep_valeur;
-	public boolean rep_actif;
 
+	private ArrayList<Boolean> rep_listeReponse;
+	private Date rep_datereponse;
+	
+	
 	public Reponse() {
-		this.rep_id = i;
-		i++;
-		this.rep_actif = false;
-
+		this.rep_listeReponse = new ArrayList<Boolean>();
+		this.rep_datereponse = new Date();
 	}
 
-	@SuppressWarnings("resource")
-	void repondre() {
-
-		System.out.println("Saisir une valeur true ou false");
-		Scanner sc = new Scanner(System.in);
-		try {
-
-			if (this.rep_actif == false) {
-				System.out.println("La question n'est plus active");
-			} else {
-				String str = sc.nextLine();
-				if (("true".equals(str)) || ("false".equals(str))) {
-					this.rep_valeur = Boolean.parseBoolean(str);
-					this.rep_actif = false;
-				} else {
-					System.out.println("Valeurs érronées ");
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("Valeurs érronées ");
-		}
+	
+	void addReponse(boolean param)
+	{
+		this.rep_listeReponse.add(param);
+		this.rep_datereponse = new Date();
 	}
-
-	@SuppressWarnings("resource")
-	void modifierReponse() {
-
-		System.out.println("Saisir une valeur true ou false");
-		Scanner sc = new Scanner(System.in);
-		try {
-
-			String str = sc.nextLine();
-			if (("true".equals(str)) || ("false".equals(str))) {
-
-				this.rep_valeur = Boolean.parseBoolean(str);
-				this.rep_actif = false;
-			} else {
-				System.out.println("Valeurs érronées ");
-			}
-
-		} catch (Exception e) {
-			System.out.println("Valeurs érronées ");
-		}
-
+	
+	
+	ArrayList<Boolean> getRepListe()
+	{
+		return this.rep_listeReponse;		
 	}
-
-	void setRepActive() {
-		this.rep_actif = true;
-	}
-
-	int getRepId() {
-		return this.rep_id;
-	}
-
-	boolean getRepValeur() {
-		return this.rep_valeur;
-	}
-
-	boolean getRepEtat() {
-		return this.rep_actif;
+	
+	
+	Date getRepDate(){
+		return this.rep_datereponse;
 	}
 }
