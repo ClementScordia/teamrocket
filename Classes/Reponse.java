002211dd6da6cf2032cee2d1ced1b469796dadd8
ClementScordia <clement.scordia@gmail.com>
2018-02-12 15:32:48 +0100

    Update Reponse.java

--- Classes/Reponse.java
+++ Classes/Reponse.java
@@ -7,28 +7,43 @@ public class Reponse {
 
 	private ArrayList<Boolean> rep_listeReponse;
 	private Date rep_datereponse;
-	
-	
-	public Reponse() {
-		this.rep_listeReponse = new ArrayList<Boolean>();
+	private int iterateur;
+
+	public Reponse(int tailleListe) {
+		this.rep_listeReponse = new ArrayList<Boolean>(tailleListe);
 		this.rep_datereponse = new Date();
+		this.iterateur = 0;
 	}
 
-	
-	void addReponse(boolean param)
-	{
-		this.rep_listeReponse.add(param);
+	public Reponse(ArrayList<Boolean> listeRepDefaut) {
+		this.rep_listeReponse = listeRepDefaut;
 		this.rep_datereponse = new Date();
+		this.iterateur = 0;
 	}
-	
+
+	void addReponse(boolean nvBooleen) {
+		if (this.iterateur <= this.rep_listeReponse.size()) {
+			this.rep_listeReponse.add(nvBooleen);
+			this.iterateur++;
+		} else {
+			System.out.println("Total de réponse atteint");
+		}
+	}
+
+	void modifierReponse(int row, boolean nvlValeur) {
+		this.rep_listeReponse.set(row, nvlValeur);
+	}
 	
 	ArrayList<Boolean> getRepListe()
 	{
-		return this.rep_listeReponse;		
+		return this.rep_listeReponse;
 	}
 	
-	
-	Date getRepDate(){
+	Date getRepDate() {
 		return this.rep_datereponse;
+	}
+
+	void setRepDate(Date nvlleDate) {
+		this.rep_datereponse = nvlleDate;
 	}
 }
