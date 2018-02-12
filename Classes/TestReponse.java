a2e64f9309998622459aecb04424c558dc048e15
ClementScordia <clement.scordia@gmail.com>
2018-02-12 15:33:16 +0100

    Update TestReponse.java

--- Classes/TestReponse.java
+++ Classes/TestReponse.java
@@ -8,70 +8,86 @@ public class TestReponse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc;
-		Reponse rep = new Reponse();
+		Reponse rep = new Reponse(1);
 		boolean test = false;
 		System.out.println(rep.getRepDate());
-		
-		
-		
-		
-		while(test==false)
-		{
+
+		while (test == false) {
 			System.out.println("Saisir une valeur true ou false");
 			sc = new Scanner(System.in);
 			try {
-					String str = sc.nextLine();
-					System.out.println(str);
-					if (("true".equals(str)) || ("false".equals(str))|| ("1".equals(str))|| ("0".equals(str))) {
-						boolean b = Boolean.parseBoolean(str);
-						System.out.println(b);
-						rep.addReponse(b);
-						test=true;
-					} else {
-						System.out.println("Valeurs érronées ::");
-					}
-			
-			}catch (Exception e) {
-				System.out.println("Valeurs érronées.");
-			}
-		}
-		
-		System.out.println(rep.getRepDate());
-		ArrayList<Boolean> tab = rep.getRepListe();
-		for(int i=0;i<tab.size();i++)
-		{
-			System.out.println(tab.get(i));
-		}
-		test=false;
-		
-		
-		while(test==false)
-		{
-			System.out.println("Saisir une valeur true ou false");
-			try { 
-				sc = new Scanner(System.in);
 				String str = sc.nextLine();
-				System.out.println(str);
-				if (("true".equals(str)) || ("false".equals(str))|| ("1".equals(str))|| ("0".equals(str))) {
-					
+				if (("true".equals(str)) || ("false".equals(str)) || ("1".equals(str)) || ("0".equals(str))) {
 					rep.addReponse(Boolean.parseBoolean(str));
-					test=true;
+					test = true;
 				} else {
 					System.out.println("Valeurs érronées ::");
 				}
-	
+
 			} catch (Exception e) {
 				System.out.println("Valeurs érronées.");
 			}
 		}
-		
+
 		System.out.println(rep.getRepDate());
-		tab = rep.getRepListe();
-		for(int i=0;i<tab.size();i++)
-		{
+		ArrayList<Boolean> tab = rep.getRepListe();
+		for (int i = 0; i < tab.size(); i++) {
 			System.out.println(tab.get(i));
 		}
-		
+		test = false;
+
+		while (test == false) {
+			System.out.println("Saisir une valeur true ou false");
+			try {
+				sc = new Scanner(System.in);
+				String str = sc.nextLine();
+			
+				if (("true".equals(str)) || ("false".equals(str)) || ("1".equals(str)) || ("0".equals(str))) {
+
+					rep.addReponse(Boolean.parseBoolean(str));
+					test = true;
+				} else {
+					System.out.println("Valeurs érronées ::");
+				}
+
+			} catch (Exception e) {
+				System.out.println("Valeurs érronées.");
+			}
+		}
+
+		System.out.println(rep.getRepDate());
+		tab = rep.getRepListe();
+		for (int i = 0; i < tab.size(); i++) {
+			System.out.println(tab.get(i));
+		}
+
+		test = false;
+
+		while (test == false) {
+			System.out.println("Saisir une valeur true ou false");
+			try {
+				sc = new Scanner(System.in);
+				String str = sc.nextLine();
+			
+				if (("true".equals(str)) || ("false".equals(str)) || ("1".equals(str)) || ("0".equals(str))) {
+
+					rep.addReponse(Boolean.parseBoolean(str));
+					test = true;
+				} else {
+					System.out.println("Valeurs érronées ::");
+				}
+
+			} catch (Exception e) {
+				System.out.println("Valeurs érronées.");
+			}
+		}
+
+		System.out.println(rep.getRepDate());
+		tab = rep.getRepListe();
+		for (int i = 0; i < tab.size(); i++) {
+			System.out.println(tab.get(i));
+		}
+
 	}
 
 }
