a163e6e47bd015df69ac5203a8c65328e151e572
Jiiinox <nathan_du_29@hotmail.fr>
2018-02-12 13:48:42 +0100

    Update Questionnaire.java

--- Classes/Questionnaire.java
+++ Classes/Questionnaire.java
@@ -6,181 +6,42 @@ import java.util.Scanner;
 
 
 public class Questionnaire {
-
-	private static int i = 0;
 	
-	public char qnr_etat;
-	public String qnr_nom;
-	public Date qnr_dateDebut;
-	public Date qnr_dateFin;
-	public int id;
-	public ArrayList listeQuestion;
+	//private String[] listeEtat = {"creation","enCours","arrete","fini"};
+	private String etat;
+	private String nom;
+	private Date dateDebut;
+	private Date dateFin;
+	
+	
+	
+
+
+	private ArrayList listeQuestion;
 	
 	//public ArrayList listeQuestionnaire;
 	
 	
-	
-	
-	public Questionnaire(/*String nom, Date dateD, Date dateF*/)
+	public Questionnaire(String nom, Date dateD, Date dateF)
 	{
-		this.id=i;
-		/*this.qnr_nom=nom;
-		this.qnr_dateDebut=dateD;
-		this.qnr_dateFin=dateF;*/
-		//this.listeQuestionnaire = new ArrayList<Questionnaire>();
-		this.listeQuestion = new ArrayList<Question>();
-		i++;
-		boolean erreur = false;
-		
-		
-		int jourD = 0;
-		int moisD = 0;
-		int anneeD = 0;
-		int jourF = 0;
-		int moisF = 0;
-		int anneeF = 0;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Saisissez un nom de questionnaire : ");
-		this.qnr_nom = sc.nextLine();
-		System.out.println("Saisissez un jour de début : ");
-		try{
-			jourD = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		System.out.println("Saisissez un mois de début : ");
-		try{
-			moisD = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		System.out.println("Saisissez une année de début : ");
-		try{
-			anneeD = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		System.out.println("Saisissez un jour de fin : ");
-		try{
-			jourF = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		System.out.println("Saisissez un mois de fin : ");
-		try{
-			moisF = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		System.out.println("Saisissez une année de fin : ");
-		try{
-			anneeF = Integer.parseInt(sc.nextLine());
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror int");
-		}
-		
-		
-		
-		SimpleDateFormat dateDeb = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat dateFin = new SimpleDateFormat("dd/MM/yyyy");
-		Date d = null;
-		try{ 
-			d = dateDeb.parse(jourD+"/"+moisD+"/"+anneeD);
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror try date deb");
-		}
-		Date f = null;
-		try{ 
-			f = dateFin.parse(jourF+"/"+moisF+"/"+anneeF);
-		} catch(Exception e){
-			erreur = true;
-			System.out.println("perror try date fin");
-		}
-		if (d.after(f))
-		{
-			System.out.println("date début est après date fin");
-			erreur = true;
-		}
-		
-		
-		
-		Calendar c = Calendar.getInstance();
-		c.setLenient(false);
-		c.set(jourD, moisD, anneeD);
-		//System.out.println("SUPORTED : "+c.isWeekDateSupported());
-		try{
-			c.getTime();
-		}catch(Exception e)
-		{
-			erreur = true;
-			System.out.println("perror date");
-		}
-		
-		Calendar c2 = Calendar.getInstance();
-		c2.setLenient(false);
-		c2.set(jourF, moisF, anneeF);
-		//System.out.println("SUPORTED : "+c2.isWeekDateSupported());
-		try{
-			c2.getTime();
-		}catch(Exception e)
-		{
-			erreur = true;
-			System.out.println("perror date");
-		}
-		
-		if (erreur == true)
-		{
-			System.out.println("Une erreur est survenu lors de la saisie");
-		}else{
-			dateDeb.format(d);
-			dateFin.format(f);
-			int choix=-1;
-			boolean erreurChoix = true;
-			
-			while (choix != 2)
-			{
-				Reponse r = new Reponse();
-				Question q = new Question(r);
-				this.listeQuestion.add(q);
-				System.out.println("Voulez-vous ajouter une autre question ?(1-Oui 2-Non)");
-				erreurChoix = true;
-				while(erreurChoix == true)
-				{
-					try{
-						choix = Integer.parseInt(sc.nextLine());
-						erreurChoix = false;
-					} catch(Exception e){
-						System.out.println("perror choix");
-						System.out.println("Mauvaise valeur saisie, ressaisissez une bonne valeur :");
-					}
-				}
-			}
-			System.out.println("fini"+d);
-			System.out.println("fini"+f);
-		}
-		
-		
-		
+		this.nom=nom;
+		this.dateDebut=dateD;
+		this.dateFin=dateF;
+		this.etat = "creation";
+		this.listeQuestion = new ArrayList<Question>();	
 	}
 	
 	
-	public void afficherQuestionnaire()
+	public String toString()
 	{
-		System.out.println("Nom du questionnaire : "+this.qnr_nom);
-		System.out.println("Date de début : "+this.qnr_dateDebut);
-		System.out.println("Date de fin : "+this.qnr_dateFin);
 		int i;
+		String res;
+		res =  "Nom du questionnaire : "+this.nom+"\nDate de dÃ©but : "+this.dateDebut+"\nDate de fin : "+this.dateFin;
 		for (i=0;i<this.listeQuestion.size();i++)
 		{
-			((Question) listeQuestion.get(i)).afficher();
+			res = res+((Question) listeQuestion.get(i)).toString();
 		}
+		return res;
 	}
 	
 	
@@ -188,16 +49,74 @@ public class Questionnaire {
 	public void changerNomQuestionnaire(String nom)
 	{
 		
-		this.qnr_nom = nom;
+		this.nom = nom;
 	}
 	
 	
-	public static void main(String[] args) {
-		
-		Questionnaire q1 = new Questionnaire();
-		q1.afficherQuestionnaire();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getEtat() {
+		return etat;
 	}
 
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+
+	public ArrayList getListeQuestion() {
+		return listeQuestion;
+	}
+
+
+	public void setListeQuestion(ArrayList listeQuestion) {
+		this.listeQuestion = listeQuestion;
+	}
+	
+	public static void main(String[] args) {
+		Date dateDeb = new Date(2017-1900,1,1);
+		Date dateFin = new Date(2018-1900,1,5);
+		Questionnaire q1 = new Questionnaire("quest1",dateDeb,dateFin);
+		System.out.println(q1.toString());
+	}
 	
 	
 }
