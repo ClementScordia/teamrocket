80e6ff0bd4daab82328af20a99a0e8396268bb97
Kermit <gwenokim@gmail.com>
2018-02-19 14:18:13 +0100

    Add files via upload

--- Classes/Gestion.java
+++ Classes/Gestion.java
@@ -1,8 +1,8 @@
-package projetsportif;
-
-import java.util.ArrayList;
+import java.sql.Date;
 
 public class Gestion {
+	
+	
 	public ArrayList<Sportif> listeSportif;
 	public ArrayList<Questionnaire> listeQuestionnaire;
 	
@@ -11,18 +11,138 @@ public class Gestion {
 		listeSportif = new ArrayList<Sportif>();
 		listeQuestionnaire = new ArrayList<Questionnaire>();
 	}
-	
-	void supprimerSportif(int id)
+
+	public boolean CreerSportif(String nom,String prenom, String pseudo, Date dateNaissance, String Sport)
 	{
-		for(int i=0;i<listeSportif.size(); i++)
-		{
-			if(listeSportif[i].getid()==id)
-			{
-				listeSportif.remove(i);
-			}
-		}
+		
+		
+		
+		return false;
+	}
+	
+	
+	public boolean CreerQuestionnaire(String nom, Date dateDebut , Date dateFin )
+	{
+		
+		return false;
+	}
+	
+	
+	public boolean CreerQuestion(String intitule, boolean qdefault) 
+	{
+		
+		return false ; 
+		
+	}
+	
+	public boolean CreerReponse(boolean defaut , boolean valeur)
+	{
+		
+		return false;
 	}
 	
 	
 	
+	
+	public boolean SupprimerSportif()
+	{
+		
+		
+		
+		return false;
+	}
+	
+	
+	public boolean SupprimerQuestionnaire()
+	{
+		
+		return false;
+	}
+	
+	
+	public boolean SupprimerQuestion() 
+	{
+		
+		return false ; 
+		
+	}
+	
+	public boolean SupprimerReponse()
+	{
+		
+		return false;
+	}
+	
+	
+	
+	public void SportifModifierNom(String Nom) 
+	{
+		
+	}
+	
+	public void SportifModifierPrenom(String prenom)
+	{
+		
+	}
+	
+	
+	public void SportifModifierDate(Date d )
+	{
+		
+	}
+	
+	
+	public void SportifModifierSport(String sport)
+	{
+		
+	}
+	
+	
+	public void QuestionnaireModifierNom(String nom)
+	{
+		
+	}
+	
+	public void QuestionnaireModifierDateDeb(Date dateDebut)
+	{
+		
+	}
+	
+	
+	public void QuestionnaireModifierDateFin(Date dateFin)
+	{
+		
+	}
+	
+	
+	public void QuestionnaireAjouterQuestion(Question q)
+	{
+		
+	}
+	
+	public void QuestionnaireSupprimerQuestion(Question q)
+	{
+		
+	}
+
+	
+	
+	
+	public void QuestionModifierIntitule(String intitule)
+	{
+		
+	}
+
+	 
+	public void QuestionModifierDefault(boolean default)
+	{
+			
+	}
+	
+
+	 
+	
+	
+	
 }
+
